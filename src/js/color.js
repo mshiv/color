a536a5d6fd7b73532ba3ef7f4dd883a5ffcb3559
mshivaprakash <shivaprakash.m91@gmail.com>
2016-12-30 21:55:21 +0100

    update to ES6 Features

---
+++ src/js/color.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Color extends React.Component {
+	constructor () {
+		super();
+		this.state = {colorText : "brown"};
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange () {
+		const value = document.getElementById("color-input").value;
+		this.setState ({colorText: value});
+	}
+
+	render () {
+	return (
+		<div id = "color-container">
+			<ColorInput colorVal = {this.state.colorText} onChange = {this.handleChange} />
+			<ColorDisplay colorDisp = {this.state.colorText} />
+		</div>
+		);
+	}
+}
+
+function ColorInput(props) {
+	return (
+		<input placeholder = "Enter color (code) here" id = "color-input" value = {props.colorVal} onChange= {props.onChange} />
+	);
+}
+
+function ColorDisplay(props) {
+	return (
+		<div id = "color-display" style = {{backgroundColor : props.colorDisp}}></div>
+	);
+}
+
+ReactDOM.render(<Color />, document.getElementById('main-container'));
